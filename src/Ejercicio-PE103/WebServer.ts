6df4f1212432de31228b1700f15c8ba17f0a53af
Ricardo Farina <alu0101117898@ull.edu.es>
2023-04-19 08:54:26 +0000

    Modificación pseudo-completada

--- src/Ejercicio-PE103/WebServer.ts
+++ src/Ejercicio-PE103/WebServer.ts
@@ -1,119 +1,37 @@
-import { spawn } from 'child_process'
-import express from 'express'
+import express from "express";
+import http from "http";
+const app = express();
+const port = 3000;
 
-/**
- * Class for a simple web server with express to execute commands.
- */
-export class WebServer {
-  /**
-   * Instance of the express server.
-   * @private
-   */
-  private server
-  /**
-   * Instance of the express application.
-   * @private
-   * @type {express.Application}
-   */
-  private app: express.Application = express()
+app.get('/weather', (req, res) => {
+  const { location } = req.query;
+  const api_key = 'c2e5e5bac366fda7739450f75f6c3b09';
 
-  /**
-   * Creates an instance of WebServer.
-   * @constructor
-   * @public
-   */
-  public constructor() {
-    this.app.get('/execmd', this.execmd)
-    this.app.get('*', this.notFound)
-  }
+  http.get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${location}`, (response) => {
+    let data = '';
 
-  /**
-   * Starts the web server.
-   * @param {number} port - The port to listen on.
-   * @public
-   * @returns {void}
-   * @example
-   * ```typescript
-   * const server = new WebServer();
-   * server.start(3000);
-   * ```
-   */
-  public start(port: number) {
-    this.server = this.app.listen(port, () => {
-      console.log(`WebServer listening on port ${port}`)
-    })
-  }
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
 
-  /**
-   * Stops the web server.
-   * @public
-   * @returns {void}
-   * @example
-   */
-  public stop() {
-    this.server.close()
-  }
+    response.on('end', () => {
+      const result = JSON.parse(data);
 
-  /**
-   * Executes a command.
-   * @param req Request of client
-   * @param res Response to client
-   * @private
-   */
-  private execmd = (req: express.Request, res: express.Response) => {
-    const cmd = req.query.cmd as string
-    let args: string[] = []
-    if (req.query.args)
-      args = (req.query.args as string).split(' ')
-    console.log(`cmd: ${cmd}, args: ${args}`)
-    try {
-      // Check if command is empty
-      /*if (!cmd) {
-        res.send('<h1>Error 400 - Bad Request</h1>')
-        return
-      }*/
+      if (result.success === false) {
+        res.status(500).json({ error: result.error.info });
+      } else {
+        res.json(result);
+      }
+    });
+  }).on('error', (error) => {
+    res.status(500).json({ error: error.message });
+  });
+});
 
-      // Execute command
-      const command = spawn(cmd, args)
-      let output = ''
-      let type = 'error'
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not found' });
+});
 
-      // Get output in case of error because of wrong command
-      command.on('error', (chunk) => {
-        output += chunk
-      })
-
-      // Get output in case of error because of wrong arguments
-      command.stderr.on('data', (chunk) => {
-        output += chunk
-      })
-
-      // Get output in case of success
-      command.stdout.on('data', (chunk) => {
-        output += chunk
-        type = 'output'
-      })
-
-      // Send output to client
-      command.on('close', (code) => {
-        console.log(`Exit code: ${code}\n`)
-        res.json({ type, output })
-      })
-    } catch (err) {
-      console.log(`Error: ${err}`)
-      res.json({ error: err })
-    }
-  }
-
-  /**
-   * Sends a 404 error.
-   * @param req Request of client
-   * @param res Response to client
-   * @private
-   */
-  private notFound = (req: express.Request, res: express.Response) => {
-    res.send('<h1>Error 404 - Not Found</h1>')
-  }
-}
-// Test
-new WebServer().start(3000)
+app.listen(port, () => {
+  console.log(`Se está ejecutando el servidor en el puerto ${port}:`);
+});
